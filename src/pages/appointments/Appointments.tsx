c8a43e603e9d8e43d5a74d2610ece7abac89d2b4
Iury <iury_tavares10@hotmail.com>
2022-04-23 20:36:09 -0300

    :sparkles: feat: implemented form

--- src/pages/appointments/Appointments.tsx
+++ src/pages/appointments/Appointments.tsx
@@ -1,9 +1,72 @@
-import React from 'react';
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable no-shadow */
+import React, { useEffect, useState } from 'react';
+import {
+  Badge, Space, Table, Title
+} from '@mantine/core';
+import axios from '../../services/api';
 
-const Appointments = () => (
-  <div>
-    Appointments
-  </div>
-);
+interface Appointment {
+  name: string,
+  birthDate: string,
+  vaccineDate: string,
+  hasVaccined: boolean
+}
 
-export default Appointments;
+const Appointment = () => {
+  const [appointment, setAppointment] = useState<Appointment[]>([]);
+
+  useEffect(() => {
+    axios.get('/appointment').then((response) => setAppointment(response.data));
+  }, []);
+
+  return (
+    <>
+      <Title>
+        Appointments
+      </Title>
+
+      <Space h="xl" />
+
+      <Table highlightOnHover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Birth Date</th>
+            <th>Vaccine Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {appointment.map((appointment, index) => (
+            <tr key={index}>
+              <td>{appointment.name}</td>
+              <td>
+                {`${new Date(appointment.birthDate).getDate()}/
+                  ${new Date(appointment.birthDate).getMonth() + 1}/
+                  ${new Date(appointment.birthDate).getFullYear()}`}
+              </td>
+              <td>
+                {`${new Date(appointment.vaccineDate).getDate()}/
+                  ${new Date(appointment.vaccineDate).getMonth() + 1}/
+                  ${new Date(appointment.vaccineDate).getFullYear()} -
+                  ${new Date(appointment.vaccineDate).getHours()}:00`}
+              </td>
+              <td>
+                <Badge
+                  variant="gradient"
+                  gradient={{ from: '#6F2DA8', to: '#510F8A', deg: 35 }}
+                >
+                  {appointment.hasVaccined ? 'Vaccined' : 'No Vaccined'}
+
+                </Badge>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+};
+
+export default Appointment;
--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,9 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { showNotification } from '@mantine/notifications';
 
-const Register = () => (
-  <div>
-    Register
-  </div>
-);
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import setHours from 'date-fns/setHours';
+import setMinutes from 'date-fns/setMinutes';
+
+import {
+  Button, TextInput, Text, Group
+} from '@mantine/core';
+import { User } from 'tabler-icons-react';
+import axios from '../../services/api';
+
+const Register = () => {
+  const hour = new Date().getHours();
+  const [birthDate, setBirthDate] = useState(new Date());
+  const [vaccineDate, setVaccineDate] = useState(
+    setHours(setMinutes(new Date(), 0), hour + 1)
+  );
+  const [name, setName] = useState<string>();
+
+  const filterPassedTime = (time: Date) => {
+    const currentDate = new Date();
+    const selectedDate = new Date(time);
+
+    return currentDate.getTime() < selectedDate.getTime();
+  };
+
+  const onSubmit = async () => {
+    const appointment = {
+      name,
+      birthDate: birthDate.toISOString(),
+      vaccineDate: vaccineDate.toISOString()
+    };
+
+    try {
+      await axios.post('/appointment', appointment);
+      showNotification({
+        message: 'Appointment created with success',
+        title: 'Success',
+        color: 'green'
+      });
+    } catch (error) {
+      showNotification({
+        message: error.response.data.message,
+        title: 'Error',
+        color: 'red'
+      });
+    }
+  };
+
+  return (
+    <div>
+      <Text weight={500} mb={8}>Full name</Text>
+      <TextInput
+        id="name"
+        value={sessionStorage.getItem('name')?.toString() ? sessionStorage.getItem('nameLoad')?.toString() : name}
+        mb={8}
+        placeholder="Your name"
+        required
+        icon={<User size={16} />}
+        onChange={(event) => {
+          setName(event.target.value);
+          sessionStorage.setItem('name', event.target.value);
+        }}
+      />
+
+      <Text weight={500} mb={8}>Birth date</Text>
+      <DatePicker
+        id="birthDate"
+        selected={birthDate}
+        onChange={(date) => date && setBirthDate(date)}
+      />
+
+      <Text weight={500} mb={8} mt={8}>Vaccine appointments</Text>
+      <DatePicker
+        id="vaccineDate"
+        selected={vaccineDate}
+        onChange={(date) => (date) && setVaccineDate(date)}
+        minDate={new Date()}
+        showTimeSelect
+        onChangeRaw={(e) => e.preventDefault()}
+        timeIntervals={60}
+        filterTime={filterPassedTime}
+        timeFormat="HH:mm"
+        dateFormat="MMMM d, yyyy HH:mm"
+      />
+
+      <Group mt={16}>
+        <Button onClick={onSubmit}>Submit</Button>
+      </Group>
+
+    </div>
+  );
+};
 
 export default Register;
